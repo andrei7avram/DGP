64ee4cfd6dd562f6f9ee448fbe748db65d168398
andrei7avram <66694499+andrei7avram@users.noreply.github.com>
2024-09-18 20:56:45 +0200

    Basic movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float groundDist;
+
+    public LayerMask groundMask;
+    public Rigidbody rb;
+    public SpriteRenderer sr;
+
+    void Start()
+    {
+    }
+
+    
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 castPos = transform.position;
+        castPos.y += 1f;
+
+        if(Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, groundMask))
+        {
+           if(hit.collider != null) {
+            Vector3 movePos = transform.position;
+            movePos.y = hit.point.y + groundDist;
+            transform.position = movePos;
+
+           }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(x, 0, y);
+        rb.velocity = moveDir * speed;
+
+        if(x!=0 && x>0)
+        {
+            sr.flipX = false;
+        }
+        else if(x!=0 && x<0)
+        {
+            sr.flipX = true;
+        }
+
+        
+    }
+    }
+
