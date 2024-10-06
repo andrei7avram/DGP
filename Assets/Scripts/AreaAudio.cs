0ad78959cede59e26079524aa76f9bc7a043b0fb
ASebastian27 <s.acujboaei@student.tue.nl>
2024-10-06 21:01:58 +0200

    updated capy animation

--- Assets/Scripts/AreaAudio.cs
+++ Assets/Scripts/AreaAudio.cs
@@ -6,6 +6,10 @@ public class AreaAudio : MonoBehaviour
 {
     public AudioSource audioSource;
 
+    void Awake() {
+        audioSource.Play();
+        audioSource.Stop();
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" && !audioSource.isPlaying)
